e13708558813ce195500d92ad4fc6a9679b2d23d
“Tabook22” <nmtabook@gmail.com>
2020-02-15 20:12:55 +0000

    testing

--- social/urls.py
+++ social/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index,server_update
 app_name = "social"
 
 urlpatterns = [
     path('', index, name="index"),
     path('index/', index, name="index"),
+    path('sup/', server_update),
 ]
