a2a1735cdc02d7c1540b7b8bf7f31f9112c91d01
Nasser Tabook <nmtabook@gmail.com>
2020-02-18 12:02:32 +0400

    fixing heroku allow hosts

--- sct/settings.py
+++ sct/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7-eoulp*k15d8vb4%rl=!(atw4s9l1t*)du-2q=j9k21)+r3w7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','https://salalahct.herokuapp.com/ ']
+ALLOWED_HOSTS = ['.herokuapp.com','https://salalahct.herokuapp.com','127.0.0.1']
 
 
 # Application definition
